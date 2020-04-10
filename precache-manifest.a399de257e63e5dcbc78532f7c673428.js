240370a321cc474cf54649d043e8255c77fdde6c
Tohaker <milesbardon@gmail.com>
2020-04-10 23:51:52 +0100

    Updates

---
+++ precache-manifest.a399de257e63e5dcbc78532f7c673428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8762150ea5083b2fb10e9ea1ad34e66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6efe65a2492ecf391974",
+    "url": "/static/css/main.0b0eb16b.chunk.css"
+  },
+  {
+    "revision": "42cb7a2a6e8c4d1aed35",
+    "url": "/static/js/2.57dd2880.chunk.js"
+  },
+  {
+    "revision": "6efe65a2492ecf391974",
+    "url": "/static/js/main.7a4e5c45.chunk.js"
+  },
+  {
+    "revision": "44552778585051448dae",
+    "url": "/static/js/runtime-main.a82ed989.js"
+  },
+  {
+    "revision": "60d093c79cf29b9a796e6a3b4bf6ba2c",
+    "url": "/static/media/egg.60d093c7.jpg"
+  }
+]);
