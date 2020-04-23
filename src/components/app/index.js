7de6b49d63ac7e58d53ba6c487352b75a925e2b6
Tohaker <milesbardon@gmail.com>
2020-04-23 12:19:18 +0100

    Change to ReactGA for analytics

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Translator from "components/translator";
 import HeaderImage from "components/image";
 import { Container, Footer } from "./styled";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-162030091-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rosegga_stone = {
   a: "eg",
@@ -110,8 +114,10 @@ const App = () => {
       </Container>
       <Footer>
         <a href="https://www.github.com/tohaker">Made with eggs by Tohaker</a>
-        <br/>
-        <a href="https://icons8.com/icon/5361/clipboard">Clipboard icon by Icons8</a>
+        <br />
+        <a href="https://icons8.com/icon/5361/clipboard">
+          Clipboard icon by Icons8
+        </a>
       </Footer>
     </>
   );
