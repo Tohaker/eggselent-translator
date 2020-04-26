eb4f9923b2ab5a8624f029ea483701791c645d37
Tohaker <milesbardon@gmail.com>
2020-04-26 21:21:32 +0000

    Working Translate component

--- src/components/translator/text/index.tsx
+++ src/components/translator/text/index.tsx
@@ -1,57 +1,29 @@
 import React from "react";
-import styled from "styled-components";
+import { TextContainer, TextBox } from "./styled";
+import { Props } from "./text.types";
 
-interface Props {
-  isInput?: boolean;
-}
+const Text = React.forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
+  const { onUpdate, isInput, value, renderProps } = props;
 
-const TextContainer = styled.div`
-  border-style: ${(props: Props) => {
-    return props.isInput ? "hidden solid hidden hidden" : "hidden";
-  }};
-
-  border-width: 1px;
-  border-color: rgba(0, 0, 0, 0.2);
-
-  width: 100%;
-  padding: 12px;
-
-  display: flex;
-`;
-
-const TextBox = styled.textarea`
-  border-width: 0;
-  resize: none;
-  width: 100%;
-  height: 100%;
-  font-family: "Roboto", arial, sans-serif;
-  font-size: 24px;
-`;
-
-const Delete = styled.div`
-  height: 24px;
-  width: 24px;
-  background-position: 0 -192px
-  background-image: url("https://ssl.gstatic.com/translate/1x_mobile.png");
-`;
-
-const Text: React.FC<Props> = ({ isInput }) => {
-  const [text, setText] = React.useState("");
-
-  const deleteText = () => {
-    setText("");
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onUpdate(e.currentTarget.value);
   };
 
+  const placeholder = !isInput ? "Transleggtion" : "";
+
   return (
-    <TextContainer isInput={isInput}>
+    <TextContainer hasBorder={isInput}>
       <TextBox
+        ref={ref}
         readOnly={!isInput}
-        value={text}
-        onChange={(e) => setText(e.currentTarget.value)}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        isInput={isInput}
       />
-      {isInput && text !== "" && <Delete onClick={deleteText} />}
+      {value !== "" && renderProps}
     </TextContainer>
   );
-};
+});
 
 export default Text;
